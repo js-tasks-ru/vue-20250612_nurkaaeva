58ccd45876a31951d4b51f1de56e68b975ceb94a
Aruzhan <aruzhanny2@halykbank.kz>
2025-07-18 14:49:20 +0500

    created removable emails

--- 03-components/30-removable-emails/EmailListItem.js
+++ 03-components/30-removable-emails/EmailListItem.js
@@ -15,10 +15,13 @@ export default defineComponent({
     },
   },
 
+
+  emits: ['delete'],
+
   template: `
     <li :class="{ marked }">
       {{ email }}
-      <button type="button" aria-label="Удалить" @click.stop>❌</button>
+      <button type="button" aria-label="Удалить" @click.stop="$emit('delete')">❌</button>
     </li>
   `,
 })
